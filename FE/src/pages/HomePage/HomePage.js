76cbcdefe6674ba3ae81682e41a6d7edeae0244a
rokas <rokasra@gmail.com>
2022-08-25 23:42:35 +0300

    homePage img's, loginPage img

--- FE/src/pages/HomePage/HomePage.js
+++ FE/src/pages/HomePage/HomePage.js
@@ -3,23 +3,39 @@ import { NavLink } from 'react-router-dom';
 import React from 'react';
 import { useAuthCtx } from '../../store/authContext';
 import toast from 'react-hot-toast';
+import unicorn from '../../assets/dab.png';
+import unicorn1 from '../../assets/one.png';
 
 const HomePage = () => {
   const { isUserLoggedIn, logout } = useAuthCtx();
   return (
     <div className={css['container']}>
-      <h1 className={css['title']}>MY FAVOURITE MOVIES</h1>
-      <ul className={css['list']}>
-        <li>Forest Gump</li>
-        <li>The Godfather</li>
-        <li>Eternal Sunshine of the Spotless Mind</li>
-        <li>Fear and Loathing in Las Vegas</li>
-        <li>Snatch</li>
-        <li>Call Me by Your Name</li>
-        <li>Top Gun</li>
-        <li>City of God</li>
-        <li>Sound of Metal</li>
-      </ul>
+      <h1 className={css['title']}>MY FAVOURITE MOVIES 🎬</h1>
+      <div className={css['homeDisplay']}>
+        <div className={css.logo}>
+          <img className={css['img-left']} src={unicorn} alt='logo' />
+        </div>
+        <div>
+          <ul className={css['list']}>
+            <li>Forest Gump</li>
+            <li>The Godfather</li>
+            <li>Eternal Sunshine of the Spotless Mind</li>
+            <li>Fear and Loathing in Las Vegas</li>
+            <li>Snatch</li>
+            <li>Call Me by Your Name</li>
+            <li>Top Gun</li>
+            <li>City of God</li>
+            <li>Sound of Metal</li>
+          </ul>
+          <div className={css.logo}>
+            <img className={css['img-right']} src={unicorn1} alt='logo' />
+          </div>
+        </div>
+        {/* <div className={css.logo}>
+          <img className={css.img} src={unicorn} alt='logo' />
+        </div> */}
+      </div>
+
       <div>
         <NavLink
           onClick={() => {
--- FE/src/pages/LoginPage/LoginPage.js
+++ FE/src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,13 @@
 import LoginForm from '../../components/LoginForm/LoginForm';
 import css from '../LoginPage/LoginPage.module.css';
+import pokemon from '../../assets/pokemon.png';
 
 function LoginPage() {
   return (
     <div className={css['login-page']}>
+      <div className={css.logo}>
+        <img className={css['img-left']} src={pokemon} alt='logo' />
+      </div>
       <h1 className={css.title}>Login Page</h1>
       <LoginForm />
     </div>
