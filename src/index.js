04dcbb42c34318f6cfef2b7a013953c047c3c727
rokas <rokasra@gmail.com>
2022-08-25 17:31:23 +0300

    empty server, routes, config, env

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const { PORT } = require('./config');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Server is running' });
+});
+
+app.all('*', (req, res) => {
+  res.status(400).send({ error: 'Page not found' });
+});
+
+app.listen(PORT, () => console.log('Listening on port', PORT));
