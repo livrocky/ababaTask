7e0f382705353e25a4d1686dd0ad06f3f095be21
rokas <rokasra@gmail.com>
2022-08-25 21:37:08 +0300

    logout button

--- FE/src/pages/HomePage/HomePage.js
+++ FE/src/pages/HomePage/HomePage.js
@@ -1,7 +1,11 @@
 import css from './HomePage.module.css';
+import { NavLink } from 'react-router-dom';
 import React from 'react';
+import { useAuthCtx } from '../../store/authContext';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
+  const { isUserLoggedIn, logout } = useAuthCtx();
   return (
     <div className={css['container']}>
       <h1 className={css['title']}>MY FAVOURITE MOVIES</h1>
@@ -12,7 +16,22 @@ const HomePage = () => {
         <li>Fear and Loathing in Las Vegas</li>
         <li>Snatch</li>
         <li>Call Me by Your Name</li>
+        <li>Top Gun</li>
+        <li>City of God</li>
+        <li>Sound of Metal</li>
       </ul>
+      <div>
+        <NavLink
+          onClick={() => {
+            logout();
+            isUserLoggedIn && toast.success('You are Logged Out');
+          }}
+          className={css['nav-link']}
+          to={'/login'}
+        >
+          Logout
+        </NavLink>
+      </div>
     </div>
   );
 };
